8e873c8794b2cd619ce68c92eb616d96881af845
Mohd Aman <44300550+ammumike@users.noreply.github.com>
2019-03-23 04:10:16 +0530

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView,TemplateView
+from . import forms
+# Create your views here.
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+class ThanskPage(TemplateView):
+    template_name = 'thanks.html'
+
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
